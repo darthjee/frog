e7765e0e697ca67a6e8c8f57b11563c9b4238be5
darthjee <darthjee@gmail.com>
2014-03-24 15:57:56 -0300

    Send request without compression

--- node/libs/redirecter/http.js
+++ node/libs/redirecter/http.js
@@ -4,7 +4,7 @@ var _ = require("underscore");
 function redirecter(config){
   redirecter = this;
   redirecter.config = _.extend({
-    port:3333,
+    port:3330,
     proxy: {
       host: "localhost",
       port: 80
@@ -14,8 +14,13 @@ function redirecter(config){
   redirecter.server = http.createServer(function(request, response){
   	response.writeHead(200, {"Content-Type":"text/html"});
   	
+  	headers = _.extend(request.headers, {
+  	  'accept-encoding': 'utf-8'
+  	});
+  	
   	var options = {
-  	  hostname: redirecter.config.proxy.host,
+      headers:headers,
+  	  hostname: redirecter.config.proxy.host,  
   	  port: redirecter.config.proxy.port,
   	  method: "get",
   	  path:"/"
@@ -23,11 +28,11 @@ function redirecter(config){
     
     var req = http.request(options, function(res) {
       res.on('data', function (chunk) {
-      	response.write(chunk);
+        response.write(chunk);
       });
 
       res.on('end', function() {
-      	response.end();
+    	  response.end();
       });
     });
     
