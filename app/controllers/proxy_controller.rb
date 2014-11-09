8b1e185a589459d4fb065b6e12082537c5fd95c1
Fernando Favini <darthjee@gmail.com>
2014-11-09 12:39:05 -0800

    WIP: add new proxy route

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,5 +1,7 @@
 class ProxyController < ApplicationController
   def new
+    @server = Proxy::Server.new
+    @server.target = Proxy::Target.new
   end
 
   def show
--- spec/controllers/proxy_controller_spec.rb
+++ spec/controllers/proxy_controller_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe ProxyController do
-  describe '#show' do
-    before do
-      get :new
-    end
-    
-    it { expect(:controller).to assign(:server) }
-  end
 end
+
