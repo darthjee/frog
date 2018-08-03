f491895484a8bc74bbb3b67ba6b9491be7d9184b
darthjee <darthjee@gmail.com>
2018-08-03 18:32:09 +0200

    Add requester

---
+++ app/utils/requester.rb
@@ -0,0 +1,43 @@
+class Requester
+  def initialize(url, path, method: :get, headers: {}, body: {})
+    @url = url
+    @path = path
+    @method = method
+    @headers = headers
+    @body = body
+  end
+
+  def call
+    case method
+    when :get
+      get
+    when :post
+      post
+    end
+  end
+
+  private
+
+  attr_reader :url, :path, :method, :headers, :body
+
+  def get
+    connector.get(path)
+  end
+
+  def post
+    connector.post(path) do |req|
+      req.body = body
+    end
+  end
+
+  def connector
+    @connector ||= build_connector
+  end
+
+  def build_connector
+    Faraday.new(url: url) do |faraday|
+      faraday.request  :url_encoded
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+end
