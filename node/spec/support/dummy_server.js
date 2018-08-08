2a3322e89b360f8230c773b1c3df739a1933bbcb
darthjee <darthjee@gmail.com>
2018-08-08 09:37:57 +0200

    Favor class format

--- node/spec/support/dummy_server.js
+++ node/spec/support/dummy_server.js
@@ -1,29 +1,27 @@
 var http = require('http'),
     _ = require('underscore');
 
-function DummyServer() {
-  _.bindAll(this, '_handleRequest');
+class DummyServer {
+  constructor() {
+    this.server = http.createServer(this._handleRequest);
+  }
 
-  this.server = http.createServer(this._handleRequest);
+  _handleRequest(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('-------------------------------------------------ok\n');
+    response.write('-------------------------------------------------dok\n');
+    console.info('finishing');
+    response.end('data');
+    console.info('finished');
+  }
+
+  start() {
+    this.server.listen(3000);
+  }
+
+  stop() {
+    this.server.close();
+  }
 }
-
-var fn = DummyServer.prototype;
-
-fn._handleRequest = function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('-------------------------------------------------ok\n');
-  response.write('-------------------------------------------------dok\n');
-  console.info('finishing');
-  response.end('data');
-  console.info('finished');
-};
-
-fn.start = function() {
-  this.server.listen(3000);
-};
-
-fn.stop = function() {
-  this.server.close();
-};
 
 module.exports = DummyServer;
