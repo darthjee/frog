54bad7e334800ff25ddd4336eff6fab86eb8bed7
darthjee <darthjee@gmail.com>
2018-08-11 20:38:29 +0200

    Test proxy with success

--- node/dummy.js
+++ node/dummy.js
@@ -1,5 +1,28 @@
 console.info('Starting this thing');
 
-Server = require('./spec/support/dummy_server');
+var Server = require('./spec/support/dummy_server');
+var EasyClient = require('./spec/support/easy_client');
+var RedirecterHttp = require('./lib/redirecter/http');
 
 new Server().start();
+
+var client = new EasyClient({
+  hostname: 'localhost',
+  port: 3300
+});
+
+var config = {
+  port: 3300,
+  proxy: {
+    host: 'localhost',
+    port: 3000
+  }
+};
+
+var redirect = RedirecterHttp(config);
+
+redirect.listen();
+
+client.call(function(data) {
+  console.info(data);
+});
