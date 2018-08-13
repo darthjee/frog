8668c3bda3547e12bcc2fe2f189c30bee9105327
darthjee <darthjee@gmail.com>
2018-08-13 09:13:45 +0200

    Add spec over POST request

--- node/spec/lib/redirecter/http_spec.js
+++ node/spec/lib/redirecter/http_spec.js
@@ -26,6 +26,7 @@ describe('Redirecter::Http', function() {
           hostname: 'localhost',
           port: this.port(),
           path: this.path(),
+          method: this.method(),
           headers: { 'X-REQUEST': '#ABC' }
         });
       },
@@ -45,7 +46,10 @@ describe('Redirecter::Http', function() {
     this.memorized('subject').stop();
   });
 
-  describe('when performing a get request', function() {
+  describe('when performing a GET request', function() {
+    beforeAll(function() {
+      this.memorize('method', 'GET');
+    });
     describe('returning success', function() {
       beforeAll(function(done) {
         this.memorize('path', '/success');
@@ -81,6 +85,63 @@ describe('Redirecter::Http', function() {
         var context = this;
 
         this.memorized('nockScope').get(/.*/)
+          .reply(500, 'error data');
+
+        this.memorized('client').call(function(response) {
+          context.response = response;
+          done();
+        })
+      });
+
+      it('returns the proxied body', function() {
+        expect(this.response.body).toEqual('error data');
+      });
+
+      it('returns the proxied status', function() {
+        expect(this.response.status).toEqual(500);
+      });
+    });
+  });
+
+  describe('when performing a POST request', function() {
+    beforeAll(function() {
+      this.memorize('method', 'POST');
+    });
+    describe('returning success', function() {
+      beforeAll(function(done) {
+        this.memorize('path', '/success');
+        var context = this;
+
+        this.memorized('nockScope').post(/.*/)
+          .reply(200, 'success data', {
+            'custom-header': 'header-value'
+          });
+
+        this.memorized('client').call(function(response) {
+          context.response = response;
+          done();
+        })
+      });
+
+      it('returns the proxied body', function() {
+        expect(this.response.body).toEqual('success data');
+      });
+
+      it('returns the proxied headers', function() {
+        expect(this.response.headers['custom-header']).toEqual('header-value');
+      });
+
+      it('returns the proxied status', function() {
+        expect(this.response.status).toEqual(200);
+      });
+    });
+
+    describe('returning error', function() {
+      beforeAll(function(done) {
+        this.memorize('path', '/error');
+        var context = this;
+
+        this.memorized('nockScope').post(/.*/)
           .reply(500, 'error data');
 
         this.memorized('client').call(function(response) {
