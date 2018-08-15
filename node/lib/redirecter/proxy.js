45fff62c22b7fc395005e0b0b9cf86800e1f1296
darthjee <darthjee@gmail.com>
2018-08-15 08:17:56 +0200

    Use singlequote

--- node/lib/redirecter/proxy.js
+++ node/lib/redirecter/proxy.js
@@ -1,5 +1,5 @@
 var http = require('http'),
-  _ = require("../underscore_ext");
+  _ = require('../underscore_ext');
 
 /**
  * Class responsible for tunnelling requests
@@ -35,7 +35,7 @@ class Proxy {
     redirecter.config = _.extend({
       port:3330,
       proxy: {
-        host: "localhost",
+        host: 'localhost',
         port: 80
       },
       before:[]
