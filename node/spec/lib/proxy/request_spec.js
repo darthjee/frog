583097bd878ffb1d43892e398bdff23a6d25c19e
darthjee <darthjee@gmail.com>
2018-08-17 16:26:55 +0200

    WIP: Add request spec

---
+++ node/spec/lib/proxy/request_spec.js
@@ -0,0 +1,57 @@
+describe('Proxy.Request', function() {
+  var proxyRequest = require('../../../lib/proxy/request'),
+    MockedResponse = require('../../support/mocked_response'),
+    nock = require('nock');
+
+  beforeAll(function() {
+    this.memorize({
+      host: 'example.com',
+      port: 3000,
+      config: function() {
+        return {
+          host: this.host(),
+          port: this.port()
+        }
+      },
+      subject: function() {
+        return proxyRequest(
+          this.config(),
+          this.originalRequest(),
+          this.response()
+        );
+      },
+      originalRequest: {
+        headers: {},
+        method: 'GET',
+        url: '/path'
+      },
+      nockScope: function() {
+        var url = 'http://' + this.host() + ':' + this.port();
+        return nock(url);
+      },
+      request: function() {
+        return this.subject().startRequest();
+      }
+    });
+  });
+
+  describe('#startRequest', function() {
+    describe('when finishing the request', function() {
+      beforeAll(function(done) {
+        this.memorize('response', function() {
+          return new MockedResponse();
+        });
+
+        this.memorized('nockScope').get(/.*/)
+          .reply(500, 'error data');
+        this.memorized('request').on('data', function() {
+        }).on('end', function() {
+          done();
+        }).end();
+      });
+
+      it('returns a Request', function() {
+      });
+    });
+  });
+});
