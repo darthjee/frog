4851ec9ba30284067e7dd4d80f4fa426bdc7f8ca
darthjee <darthjee@gmail.com>
2018-08-29 18:19:37 +0200

    Generate app

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-var express = require('express'),
-  app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.get('/', function(_request, response) {
-  response.redirect('/home');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get('/home', function(_request, response) {
-  response.send('Home');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.get('/servers', function(_request, response) {
-  response.send('servers');
-});
-
-app.get('/servers/:id', function(_request, response) {
-  response.send('servers id');
-  console.info(_request.params);
-});
-
-app.get(/.*/, function(_request, response) {
-  response.send('Star');
-});
-
-app.listen(3000);
-
-
+module.exports = app;
