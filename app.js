5c6eb434ebd1b320f637c10f4747bd16ed3aba24
darthjee <darthjee@gmail.com>
2018-08-29 18:18:55 +0200

    Testing

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-var proxy = require('./lib/redirecter/http.js');
+var express = require('express'),
+  app = express();
 
-proxy({
-  before: function(){
-    return true;
-  }
-}).listen();
+app.get('/', function(_request, response) {
+  response.redirect('/home');
+});
+
+app.get('/home', function(_request, response) {
+  response.send('Home');
+});
+
+app.get('/servers', function(_request, response) {
+  response.send('servers');
+});
+
+app.get('/servers/:id', function(_request, response) {
+  response.send('servers id');
+  console.info(_request.params);
+});
+
+app.get(/.*/, function(_request, response) {
+  response.send('Star');
+});
+
+app.listen(3000);
+
+
